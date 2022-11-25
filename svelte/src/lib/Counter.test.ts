7c321d03107d75bd188bbf35acde336509109865
andrew <andrew@pixotech.com>
2022-11-25 07:57:42 -0600

    Add unit test to assert that custom event was fired when counter button is clicked

--- svelte/src/lib/Counter.test.ts
+++ svelte/src/lib/Counter.test.ts
@@ -18,4 +18,15 @@ describe("Counter", () => {
         let text = await result.findByTestId("counter-value");
         expect(text.textContent).toEqual("1");
     })
+    it("dispatches a custom event when the button is clicked", async () => {
+        let eventWasCalled = false;
+        result.container.addEventListener("on-increment", () => {
+            eventWasCalled = true;
+        });
+
+        let button = await result.findByText("Increment");
+        await fireEvent.click(button);
+
+        expect(eventWasCalled).toEqual(true);
+    })
 })
